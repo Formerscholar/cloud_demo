fa39d0ffabf345a99d3f6ea27a78ae9135000e58
Formerscholar <2319315514@qq.com>
2021-06-28 13:05:28 +0800

    init

---
+++ cloud-provider-payment8001/src/main/java/com/chad/cloud/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package com.chad.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 付款main8001
+ *
+ * @author Chad
+ * @date 2021/06/28
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentMain8001.class, args);
+	}
+}
