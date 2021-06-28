58a985539ee7c01157fa20be3fc367a05c791876
Formerscholar <2319315514@qq.com>
2021-06-28 17:07:37 +0800

    chad

---
+++ cloud-consumer-order80/src/main/java/com/chad/cloud/controller/OrderController.java
@@ -0,0 +1,32 @@
+package com.chad.cloud.controller;
+
+import com.chad.cloud.entity.Payment;
+import com.chad.cloud.helps.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("consumer")
+@Slf4j
+public class OrderController {
+	public static final String PaymentSrv_URL = "http://localhost:8001";
+
+	@Resource
+	private RestTemplate restTemplate;
+
+	@GetMapping("/payment/create")
+	public Result<Payment> create(Payment payment) {
+		return restTemplate.postForObject(PaymentSrv_URL + "/payment/create", payment, Result.class);
+	}
+
+
+	@GetMapping("/payment/get/{id}")
+	public Result<Payment> getPayment(@PathVariable("id") Long id) {
+		return restTemplate.getForObject(PaymentSrv_URL + "/payment/get/" + id, Result.class, id);
+	}
+
+}
